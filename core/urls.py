62732d609dd26a0a939c254199de8f660c3423bc
Saurabh Yadav <sauraj.contact@gmail.com>
2025-02-17 16:50:53 +0530

    Refactor URL patterns to conditionally serve static and media files in debug mode

--- core/urls.py
+++ core/urls.py
@@ -12,12 +12,9 @@ urlpatterns = [
   path('api/', include('admin_panel.urls')),
 ]
 
-# if settings.DEBUG:
-#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#   urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 def register_functions():
   register_all()
